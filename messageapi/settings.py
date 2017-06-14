7b35c955617e34f2852beaea5f3fd1b59c286fc6
Shubham Shrivastav <shubham_shrivastav@persistent.co.in>
2017-06-14 13:20:35 +0530

    added allowed hosts

--- messageapi/settings.py
+++ messageapi/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/assets"),
+    os.path.join(PROJECT_DIR, 'static\\assets'),
 )
