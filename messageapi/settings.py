d33b7ea72c67c15d6e929c18b7cd3f95aa2e3b3e
Shubham Shrivastav <shubham_shrivastav@persistent.co.in>
2017-06-14 12:50:48 +0530

    added allowed hosts

--- messageapi/settings.py
+++ messageapi/settings.py
@@ -15,7 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/assets')
+STATIC_ROOT = os.path.join(PROJECT_DIR, '../static')
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_DIR, 'static'),
+    os.path.join(PROJECT_DIR, 'static/asstes'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -26,7 +30,7 @@ SECRET_KEY = '$vylrtjfz&q6jfv5tvsv&$i2d_i!*r&3g6s-#71q$q7rxwxs_v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['smsify.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
