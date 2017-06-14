0980352fa9dc332bfbeceb4aea2606e9069b2846
Shubham Shrivastav <shubham_shrivastav@persistent.co.in>
2017-06-14 13:41:54 +0530

    added allowed hosts

--- messageapi/demo.py
+++ messageapi/demo.py
@@ -1,6 +1,6 @@
 import os
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, '..')
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 print(STATIC_ROOT)
