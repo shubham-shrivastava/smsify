530ab5a4b6b9f63d108cf7ba54e4851e0dcdf220
Shubham Shrivastav <shubham_shrivastav@persistent.co.in>
2017-06-14 13:23:33 +0530

    added allowed hosts

--- messageapi/settings.py
+++ messageapi/settings.py
@@ -122,5 +122,5 @@ STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, 'static\\assets'),
+    os.path.join(PROJECT_DIR, 'static/assets'),
 )
