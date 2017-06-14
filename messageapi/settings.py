7931d783f87995fca94bbd75b0060960fd42b53e
Shubham Shrivastav <shubham_shrivastav@persistent.co.in>
2017-06-14 12:56:22 +0530

    added allowed hosts

--- messageapi/settings.py
+++ messageapi/settings.py
@@ -14,12 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, '../static')
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, 'static'),
-    os.path.join(PROJECT_DIR, 'static/asstes'),
-)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -124,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static/assets"),
+)
