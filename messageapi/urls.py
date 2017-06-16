4af49616651db56083a88c0af7762dd033a11b4c
Shubham Shrivastav <shubham_shrivastav@persistent.co.in>
2017-06-16 11:09:23 +0530

    static

--- messageapi/urls.py
+++ messageapi/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^myadmin/', admin.site.urls),
     url(r'^', include('smsdetails.urls')),
 ]
 
