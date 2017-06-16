b8381dfb0293a082db28086360cfa22ae6c32a1a
Shubham Shrivastav <shubham_shrivastav@persistent.co.in>
2017-06-16 19:25:53 +0530

    validation

--- messageapi/settings.py
+++ messageapi/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -82,6 +82,9 @@ DATABASES = {
     }
 }
 
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
