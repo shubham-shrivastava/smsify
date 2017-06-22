cb209e5652557dd1015ed857b49308d99664e24d
Shubham Shrivastav <shrivastava.shubham219@gmail.com>
2017-06-22 12:41:02 +0000

    changed number

--- smsdetails/models.py
+++ smsdetails/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import Permission, User
 from django.conf import settings
 
 phone_regex = RegexValidator(
-    regex=r'^\+?1?\d{10}$', message="Phone number must be entered in the format: '+999999999'. 10 digits allowed.")
+    regex=r'^(\+?1?\d{10})|(\+?1?\d{13})$', message="Phone number must be entered in the format: '+999999999'. 10 digits allowed.")
 # Create your models here.
 
 #User._meta.get_field('email')._unique = True
